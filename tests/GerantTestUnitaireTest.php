4311bdc5bb0e92f12cda19d6993c6042f1462c62
Fofomalo <maloflorent3@gmail.com>
2022-08-29 14:43:03 +0200

    new file testunitaire, hotel, room

--- tests/GerantTestUnitaireTest.php
+++ tests/GerantTestUnitaireTest.php
@@ -8,7 +8,7 @@ use App\Entity\Gerant;
 
 class GerantTestUnit extends TestCase
 {
-    public function testIsTrue(): void
+   public function testIsTrue(): void
     {
         $gerant = new Gerant();
         $gerant ->setName('Nom')
@@ -19,5 +19,25 @@ class GerantTestUnit extends TestCase
         $this->assertTrue($gerant->getEmail()==='for@test.com');
         $this->assertTrue($gerant->getPassword()==='password');
 
+    }
+    public function testIsFalse():void
+    {
+        $gerant = new Gerant();
+        $gerant ->setName('gerant')
+                ->setEmail('for@test.com')
+                ->setPassword('mypassword');
+        $this -> assertFalse($gerant->getName() ==='name');
+        $this -> assertFalse($gerant->getEmail() ==='false@test.com');
+        $this -> assertFalse($gerant-> getPassword()==='password');
+    }
+    public function testIsEmpty()
+    {
+        $gerant = new Gerant();
+        $gerant ->setName('empty')
+                ->setEmail('empty')
+                ->setPassword('empty');
+        $this -> assertEmpty($gerant -> getName()==='');
+        $this -> assertEmpty($gerant -> getEmail()==='');
+        $this -> assertEmpty($gerant -> getPassword()==='');
     }
 }
